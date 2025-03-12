1a50015b134483e48eb7e8519380aa8ea2ef84fa
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-03-12 23:53:34 +0530

    conditionals code

---
+++ if_elif_else.py
@@ -0,0 +1,84 @@
+# operators 
+# variables 
+# datatypes
+# functions 
+
+# input  
+# conditionals  
+
+# username= input("enter your name:")
+# mob=int(input("enter mobile number: "))
+# print(type(username),type(mob))
+
+# condtional statements
+# if  
+# else 
+# elif 
+# match  
+
+# num=int(input("enter a number: "))
+
+# if num<0:
+#     print("number is negitive")
+# else: 
+#     print("non negitive number")
+    
+
+
+# if username=="mani@123" and  password=="secret_123":
+#     print("valid user")
+# else:
+#     print("invalid username") 
+
+
+
+
+# username=input("enter the username: ") 
+# password=input("enter the password: ")
+
+
+# if username=="mani@123" and password=="secret_123" :
+#     print("welcome user")
+# else:
+#     print("invalid user")
+
+
+# color=input("enter the color")
+
+# if color=="blue":
+#     print("buy") 
+# elif color=="black":
+#     print("buy black") 
+# elif color=="maroon":
+#     print("buy maroon")
+# elif color=="white":
+#     print("buy white")
+# else:
+#     print("dont buy")
+
+
+# if color=="blue":
+#     print("blue") 
+# elif color=="black":
+#     print("black")
+# elif color=="maroon":
+#     print("buy maroon")
+# else:
+#     print("dont buy")
+    
+
+# username=input("enter the username: ") 
+# password=input("enter the password: ") 
+# if username=="username" and password=="password":
+#     print("welcome user")
+# elif username=="username" and password!="password":
+#     print("invalid password")
+# elif password=="password" and username!="username":
+#     print("invalid username")
+# else:
+#     print("invalid credentials")   
+
+num=1
+print(type(num))
+
+    
