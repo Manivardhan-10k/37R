8235256868abc7608a856b352f69af9ff183d9cb
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-03-13 09:36:44 +0530

    nested conditonals and match case

---
+++ nested.py
@@ -0,0 +1,110 @@
+# conditional statements
+# if  
+# else 
+# elif 
+
+# nested if
+# indian  18 
+# citizen="american"
+# age=18
+
+# if citizen=="indian":
+#     if age>18:
+#         print("eligible to vote")
+#     else:
+#         print("age not sufficient")
+# else:
+#      print("you are not indian")
+
+# bill>10000 sbi-5% hdfc-10% 
+
+# card="HDFC"
+# bill=10000 
+
+# if bill>=10000:
+#     if card=="SBI":
+#         print("discount is",bill*5/100)
+#     elif card=="HDFC":
+#         print("discount is ",bill*10/100)
+#     else:
+#         print("no discount")
+# else:
+#     print("add more items worth of ",10000-bill," to get discount")
+    
+
+
+
+# match
+
+
+
+# match val 
+#   case 1:
+#       print()
+#   case 2: 
+#       print()
+# day=12
+# match day:
+#     case 0:
+#         print("sunday")
+#     case 1:
+#         print("monday")
+#     case 2:
+#         print("tuesday")
+#     case 3:
+#         print("wednesday")
+#     case 4:
+#         print("thursday")
+#     case 5:
+#         print("friday")
+#     case 6:
+#         print("saturday")
+#     case _:
+#         print("invalid day")
+
+
+
+# if 2+5j:
+#     print("hello")
+# else:
+#     print("false")
+
+
+# Truthsy values, falsy values
+# True,
+# -infinity...infinity
+# " " -- non empty string 
+
+
+# Falsy values
+#[]
+#{}
+#()
+#0
+#""
+#False
+
+
+
+
+
+
+
+# " ",True , {jksd},[djks],(),-9,9
+
+
+# name=input("enter your name")
+# print(name,type(name))
+
+# if name:
+#     print(name)
+# else:
+#     print("no name")
+
+
+#loops 
+# to do the repeated task easily
+# for 
+# while 
+
+# print("python is great")
