be911eb603d9e6d85fdcb25be163c0d0c865ce64
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-03-17 09:44:09 +0530

    added while and control statements

---
+++ loops.py
@@ -0,0 +1,116 @@
+# LOOPS 
+# it is used to do a repetetive task 
+# print("pyhton is great")
+
+# For 
+# it is used  when the iterations are finite
+# While 
+# it is used  when the iterations are not known
+
+
+#50 
+# for i in range(1,51,1):
+
+# for --->name of the loop 
+# i -->iterator  
+# in --> keyword 
+
+# range ()
+# start  ->1
+# stop  ->50
+# step  ->1
+
+
+# for i in range(1,101,1):#1
+#     print(i,"python")
+
+# 1.assigmmentm  #1
+# 2.condition   # <51
+# 3.executes the block of code
+# 4.increment  #2 ......#50
+
+
+# for hello in range(1,11,1):
+#     print(hello)
+# name='mani'
+# for i in range(2,102):
+#     print(i,name)
+
+# limit=int(input("enter the number"))
+# for i in range(1,limit+1):
+#     if i%2==0:
+#         print(i)
+# num=int(input("enter a number")) #5
+
+# for i in range(1,21,1):
+#    print(num,"X",i,"=",num*i)
+
+# 100 -1 
+# for i in range(100,0,-1):
+#     print(i)
+
+#reverse iteration 
+#start
+#stop
+#step --> should be in negitive values
+
+
+# for i in range(1,6,1):##1 2
+#     for j in range(1,21,1):
+#         print(i,"X",j,"=",i*j)##1*1 =1  1*2=2  1*20=20 
+
+
+
+
+#i need to print tables from 14-20 
+
+# each table contains 10 multiples 1 ....10
+
+
+# for num in range(14,21,1):##14
+#     for mul in range(1,11,1):#1 2 3 ....10
+#         print(num,"X",mul,"=",num*mul)#14*1=14 .....14*10=140
+
+# for i in range(23):
+#     print(i)
+
+
+# 1.assignment  
+#2.condition 
+# 3. code execution 
+# 4.increment/
+# 1
+# 2 2
+# 3 3 3 
+# 4 4 4 4
+# 5 5 5 5 5
+
+# r=5 
+# for row in range(1,6):
+#     s=""
+#     for col in range(1,row+1):
+#       s+="* "
+#     print(s)
+
+# num=int(input("enter a num"))#13 
+# fact=0
+# for i in range(1,num+1,1):# 1----13
+#     if num%i==0:
+#         fact=fact+1 #1 2
+# if fact==2:
+#     print(num,"is prime")
+# else:
+#     print("not prime") 
+
+
+# 25 
+# 1 5 25 
+# 36 
+# 1 2 3 4 6 9 12 18 36 
+
+# 1- 5
+# 1*2 =2 
+# 1*4 =4 
+# 1*10 =10
+
+# for i in range(1,6):
---
+++ while_cs.py
@@ -0,0 +1,133 @@
+# for 
+# when the iterations are finite
+# while 
+#when the iterations are not known
+
+
+# for i in range(1,51,1):#
+#     print("hi")
+
+# initialization
+# condition
+#execution of code
+#increment
+
+
+
+
+
+
+
+
+##prime or not  
+#1 2 4 5  10 20 25 50 100
+# 1 .  . .     ... 17
+#1 .....73 
+
+# num should be divisible by 1 and itself
+# num=100
+# factors=0
+# for i in range(1,num+1,1):#1 2   ... 18
+#     if num%i==0:##17%1==0 17%2==0 ..... 17%17==0
+#         factors+=1#0+1   1+1  2
+#         print(i)
+# if factors==2:
+#     print(factors)
+# else:
+#     print('not prime')
+
+
+
+
+# True , -1 , 1, {},[],(),"s"
+
+
+# while 
+
+# num=int(input("enter the num"))#50
+# count=0
+# while True:  #10<=100
+#     print(num)
+#     count+=1
+#     num=num+1
+# print(count)
+
+
+
+
+
+
+
+
+#2 3 5 7 11 13  17 19 23 29 31 37 41 43 47 51 53 59 61........97 
+
+# num=105
+# #to find next prime
+# prime_not_found=True  ##flag for condition
+# while prime_not_found: #False
+#     num+=1 #106  107
+#     fact=0
+#     for i in range(1,num+1,1):
+#         if num%i==0:
+#             fact+=1
+#     print(num,fact)
+#     if fact==2:
+#         prime_not_found=False
+#         print(num,"is next prime")
+
+
+
+##control statements
+# break
+# it stops the iteration of the loop
+##it only works for loops
+
+
+
+#continue 
+# it skips the iteration if the condition is true
+
+
+#pass 
+#it acts as a placeholder for the block of code
+
+
+
+# num=7
+# for  i in range(1,11,1): 
+#    print(i) #1 2 3 4 5 6 
+#    if i==num:
+#       break
+
+
+
+# for i in range(1,11,1):
+#     print(i)
+#     if i%2==0:
+#         continue
+
+
+
+# if "":
+#     pass
+# else:
+#     print("false")
+
+
+# loop controls
+# break 
+# continue 
+
+# condition control
+# pass
+
+
+
+
+# find the second prime of given number
+#13 ==19
+
+# break the loop if the i matches with the given number
+
+# continue
+# 3 6 9 12 15 18 21 ....99
