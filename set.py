31edd919c2448e41cc849584c1df9d60ed302ed9
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-03-21 10:27:46 +0530

    set datatype and methods

---
+++ set.py
@@ -0,0 +1,122 @@
+# List 
+#Dict 
+# my_dictionary={
+#     "name":123,
+#     "mob":123,
+#     "name":"mani"
+# }
+# set 
+# it is denoted with {}
+## it stores only the unique values 
+
+##homogenous 
+##heterogenous
+
+# my_set={1,2,3,1,2,1,1,1,1,1,1,3,5,6,7,8,9,4,"hi","hello",10}
+# my_list=[1,2,3,4,5,1,2,3,4,5]
+# print(my_set)
+# print(type(my_set))
+# new_list=set(my_list)
+# print(list(new_list))
+# print(len(my_set))
+
+# for ele in my_set:
+#     print(ele)
+
+
+#add
+# my_set.add(13)
+# my_set.add("how")
+
+##remove
+# my_set.remove("how")
+
+
+#discard
+# my_set.discard("hello")
+# my_set.discard("hey")
+
+#pop() 
+# my_set.pop()
+# my_set.pop()
+# first=my_set.pop()
+# print(my_set.clear())
+
+
+#union
+# set1={1,2,3}
+# set2={2,3,4,5}
+# set3={6,7,}
+
+
+
+# print(set1.union(set2,set3))
+
+
+# set1={1,2}
+# set2={2,3,4,5}
+# set3={6,7,2}
+
+#intersection
+# print(set1.intersection(set2,set3))
+#difference
+# print(set2.difference(set1))
+
+#symmetric difference
+# print(set1.symmetric_difference(set2))
+
+
+#copy()
+# new=set1.copy()
+# print(new)
+
+
+##FROZEN SET
+# new=frozenset([1,2,3,4,"hi"])
+# new1=frozenset([1,5,6,2,3,"hi"])
+
+
+# print(new.difference(new1))
+# print(new.symmetric_difference(new1))
+
+#copy 
+#difference
+#symmetric difference
+#union
+#intersection
+
+#isdisjoint
+# print(new.isdisjoint(new1))
+
+#issubset
+# print(new1.issubset(new))
+
+#issuperset
+# print(new.issuperset(new1))
+
+
+
+##Problem solving
+# my_list=[2,5,7,4,3,8,9]  ##2* 3* 5 *7
+# input_list=[]
+# val= int(input("enter the number of elements: "))
+# for i in range(1,val+1,1):
+#     inp=int(input("enter the number: "))
+#     input_list.append(inp)
+# print(input_list)
+
+
+
+
+#take a set of numbers 
+my_set={1,2,3,4,5,6,7,8,9,10}
+print(my_set)
+
+# new={}
+
+#research  
+
+
+#take the lead
+
+
