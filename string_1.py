20c465f7b144ef22eb5514d3f3ffadea06a98e1d
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-03-24 10:46:50 +0530

    intro to strings and string methods

---
+++ string_1.py
@@ -0,0 +1,126 @@
+# string:
+# it is a collection of characters enclosed in '' ""  '''''' """"""
+# it is a primitive datatype
+# it can only store a single value
+#it is immutable
+
+
+#single line string
+# name='name'
+# name2="name2"
+
+
+#multi line string
+# name3="""name3"""
+# name4='''name4'''
+
+# print(type(name))
+# print(type(name2))
+# print(type(name3))
+# print(type(name4))
+
+
+# my_name="manivardhan"
+# about_me="""i am 
+# manivardhan   """
+
+# print(about_me)
+
+
+# role='"      "'
+
+# print(len(role))
+# print(role[-1])
+
+# role[0]="i"
+# print(role[0])
+
+
+# name=input("enter the name: ")
+# name=name.strip()
+# mat="manivardhan"
+# print(mat==name)
+#upper()
+# print(name.upper())
+#lower
+# print(name.lower())
+
+#strip
+# print(name.strip())
+
+
+# about="      i am learning python      "
+# #lstrip()
+# print(about.lstrip())
+# #rstrip()
+# print(about.rstrip())
+
+
+# string="my name is manivardhan namE"
+
+# print(string.replace("m","M"))
+# print(string.replace("namE","1234156788"))
+# string=string.replace("namE","1234156788")
+# print(string)
+
+
+
+#startswith
+# name="Harsha"
+# print(name.startswith("H"))
+
+# #endswith
+# print(name.endswith("a"))
+
+
+
+
+# string="p for apple"
+# print(string.find("y"))
+
+
+
+# para="there used  to be a king"
+# print(para.replace("queen","hello"))
+
+# print(skills.lower().find("php"))
+# skills="html , css , js,python,c, c++,java,c#,bootstrap,sql,Php"
+
+#count
+# print(skills.count("P"))
+
+# sentence="converts the first letter of the word in the sentence"
+# #title()
+# sentence=sentence.title()
+# print(sentence)
+
+# #captalize
+# name="my name is manivardhan"
+# print(name.capitalize())
+
+
+# string="hydErAbAd" #eaa
+# string=string.lower()
+# string=string.upper()
+
+#0 "" false none
+# vowels="aeiou"
+# for i in range(0,len((string)),1):
+#     ch=string[i]#h
+#     # if ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u":
+#     #     print(ch)
+#     # # print(ch==vowels) ## h==aeiou
+#     # print(vowels.find(ch),ch)
+#     # if vowels.find(ch)!=-1:
+#     #     print(ch)
+
+# for i in string:
+#     if i in vowels:
+#         print (i)
+    
+
+
+
+string="I am mani, this is python" 
+#replace the first value   "t" "T"
+
